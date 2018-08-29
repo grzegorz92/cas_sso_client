b19db70f74309c4a9d9d0299f676f384b410808f
grjk <grjk@ailleron.com>
2018-08-29 15:06:39 +0200

    added etc folder, small fixes

--- src/main/java/cas/client/cas_client/config/FilterConfig.java
+++ src/main/java/cas/client/cas_client/config/FilterConfig.java
@@ -1,30 +1,30 @@
-package cas.client.cas_client.config;
-
-import com.kakawait.spring.security.cas.client.CasAuthorizationInterceptor;
-import com.kakawait.spring.security.cas.client.ticket.ProxyTicketProvider;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.security.cas.ServiceProperties;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.ForwardedHeaderFilter;
-
-@Configuration
-public class FilterConfig {
-
-    @Bean
-    FilterRegistrationBean forwardedHeaderFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    RestTemplate casRestTemplate(ServiceProperties serviceProperties, ProxyTicketProvider proxyTicketProvider) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getInterceptors().add(new CasAuthorizationInterceptor(serviceProperties, proxyTicketProvider));
-        return restTemplate;
-    }
-}
+//package cas.client.cas_client.config;
+//
+//import com.kakawait.spring.security.cas.client.CasAuthorizationInterceptor;
+//import com.kakawait.spring.security.cas.client.ticket.ProxyTicketProvider;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.Ordered;
+//import org.springframework.security.cas.ServiceProperties;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.filter.ForwardedHeaderFilter;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Bean
+//    FilterRegistrationBean forwardedHeaderFilter() {
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+//        filterRegistrationBean.setFilter(new ForwardedHeaderFilter());
+//        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return filterRegistrationBean;
+//    }
+//
+//    @Bean
+//    RestTemplate casRestTemplate(ServiceProperties serviceProperties, ProxyTicketProvider proxyTicketProvider) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.getInterceptors().add(new CasAuthorizationInterceptor(serviceProperties, proxyTicketProvider));
+//        return restTemplate;
+//    }
+//}
